d2ba16ed8e8c993c3a977c594322dc16c1a70e19
adam <adamaly93@gmail.com>
2024-10-27 14:37:50 +0000

    Long winded way to sort streams.

--- Streams/src/App.java
+++ Streams/src/App.java
@@ -12,9 +12,13 @@ public class App {
 
 
         // Declarative approach
-        movies.stream()
-            .sorted(Comparator.comparing(Movie::getLikes))
-            .forEach(m -> System.out.println(m.getTitle()));
+        // movies.stream()
+        //     .sorted(Comparator.comparing(Movie::getLikes))
+        //     .forEach(m -> System.out.println(m.getTitle()));
         
+
+        movies.stream()
+        .sorted((a, b) -> b.getLikes() - a.getLikes())
+        .forEach(m -> System.out.println(m.getTitle()));
     }
 }
