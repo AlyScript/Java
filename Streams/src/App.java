4d84cdd278a749bdfbabe50a0cea574680ad40ea
adam <adamaly93@gmail.com>
2024-10-27 14:30:47 +0000

    Sorting Streams

---
+++ Streams/src/App.java
@@ -0,0 +1,20 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List<Movie> movies = List.of(
+            new Movie("c", 10),
+            new Movie("b", 15),
+            new Movie("a", 20)
+        );
+
+
+
+        // Declarative approach
+        movies.stream()
+            .sorted(Comparator.comparing(Movie::getLikes))
+            .forEach(m -> System.out.println(m.getTitle()));
+        
+    }
+}
