ab6c9c9a3860794e38a8c5d2f54f36a6b1afbbea
adam <adamaly93@gmail.com>
2024-10-30 16:55:24 +0000

    Thread Ex. 2

--- Threads/src/ThreadExample2.java
+++ Threads/src/ThreadExample2.java
@@ -2,11 +2,14 @@ public class ThreadExample2 {
     
     public static void main(String[] args) {
         Runnable runnable = () -> {
-            System.out.println("Running thread...");
-            System.out.println("Stopping thread...");
+            String threadName = Thread.currentThread().getName();
+            System.out.println(threadName + " is running.");
         };
 
-        Thread thread = new Thread(runnable);
+        Thread thread = new Thread(runnable, "Thread1");
         thread.start();
+
+        Thread thread2 = new Thread(runnable, "Thread2");
+        thread2.start();
     }
 }
